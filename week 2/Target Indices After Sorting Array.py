674262b3cdf5274843556aea911f458c9ab4b1e5
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-01-06 00:43:31 +0300

    target indices

--- week 2/Target Indices After Sorting Array.py
+++ week 2/Target Indices After Sorting Array.py
@@ -2,9 +2,7 @@ class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
         num=[]
-        for i in nums:
-            if i != target:
+        for i in range(len(nums)):
+            if nums[i]==target:
                 num.append(i)
-                break
-        num.append(target)
         return num
