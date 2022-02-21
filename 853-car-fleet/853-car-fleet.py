edd0d8443be52437c87b60787b3711fd6d494893
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-02-21 09:26:46 +0300

    Time: 1004 ms (74.61%), Space: 36.9 MB (24.41%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack, d = [], {}
+        for i in range(len(speed)):
+            d[position[i]] = speed[i]
+        position=sorted(position)
+        for j in position:
+            dist = (target - j) / d[j]
+            while stack and (stack[-1] <= dist):
+                stack.pop()    
+            stack.append(dist)
+        return len(stack)
+            
+        
+    
