4bb7ab686308c0595cf533d6337cb436970d93c0
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-02-28 16:55:56 +0300

    Time: 51 ms (69.01%), Space: 15.1 MB (33.24%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -8,10 +8,9 @@ class Solution:
     def checkSum(self, root, targetSum):
         if not root:
             return False
-        targetSum -= root.val
+        targetSum-=root.val
         if not root.left and not root.right:
             return targetSum==0
-        return self.checkSum(root.right, targetSum) or self.checkSum(root.left, targetSum)
-    
+        return self.checkSum(root.left, targetSum) or self.checkSum(root.right, targetSum)
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         return self.checkSum(root, targetSum)
