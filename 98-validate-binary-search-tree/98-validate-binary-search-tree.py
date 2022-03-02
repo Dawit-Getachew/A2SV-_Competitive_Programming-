2acab7e4f736bfaea5406195139e652b8db7e393
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-03-02 16:40:23 +0300

    Time: 66 ms (46.99%), Space: 17.1 MB (9.12%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -11,16 +11,19 @@ class Solution:
         def traverse(r):
             if r is None:
                 return
-            
             traverse(r.left)
-        
             traversed.append(r.val)
-            
             traverse(r.right)
-              
+            
         traverse(root)
         
-        if sorted(list(set(traversed))) == traversed:
-            return True
-        else:
-            return False
+        for i in range(1, len(traversed)):
+            if traversed[i] > traversed[i-1]:
+                pass
+            else:
+                return False
+        return True
+        # if sorted(list(set(traversed))) == traversed:
+        #     return True
+        # else:
+        #     return False
