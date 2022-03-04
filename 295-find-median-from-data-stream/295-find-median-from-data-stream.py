4dbe720a65264b04c38b0bd57e56ebc015cb5243
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-03-04 16:24:53 +0300

    Time: 500 ms (99.12%), Space: 35.6 MB (87.60%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -2,33 +2,22 @@ import heapq as h
 class MedianFinder:
 
     def __init__(self):
-        self.arr = []
+        self.rightHalf = []
+        self.leftHalf = []
 
-    def addNum(self,num: int) -> None:
-        self.arr.append(num)
-
-    def findMedian(self) -> float:
-        n = len(self.arr)
-        if n % 2 == 0:
-            self.arr.sort()
-            i = n//2
-            return (self.arr[i] + self.arr[i-1])/2
-            # temp = []
-            # for _ in range((len(self.arr)//2 + 1)):
-            #     temp.append(h.heappop(self.arr))
-            # res1 = (temp[-1] + temp[-2])/2  
+    def addNum(self, num: int) -> None:
+        if len(self.leftHalf) > len(self.rightHalf):
+            temp = h.heappushpop(self.leftHalf, -num)
+            h.heappush(self.rightHalf, -temp)
         else:
-            self.arr.sort()
-            return self.arr[n//2]
-            return
-#             temp =[]
-#             for _ in range((len(self.arr)//2 + 1)):
-#                 temp.append(h.heappop(self.arr))
-        
-#             res = temp[-1]
-#             return res
-        return 0
-            
+            temp = h.heappushpop(self.rightHalf, num)
+            h.heappush(self.leftHalf, -temp)
+    def findMedian(self) -> float:
+        if len(self.leftHalf) == len(self.rightHalf):
+            temp = (-self.leftHalf[0] + self.rightHalf[0])/2
+            return temp
+        else:
+            return -self.leftHalf[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
