7849945e8462336b56ebc5e949af0122011ea686
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-03-05 00:14:07 +0300

    Time: 131 ms (62.74%), Space: 18.8 MB (49.51%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,20 +1,6 @@
 import heapq as h
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         arr = Counter(nums)
-#         kval = []
-#         ans = []
-#         for key in arr:
-#             kval.append((arr[key] , key))
-            
-#         kval = [(-x,y) for x,y in kval]
-#         heapq.heapify(kval)
-    
-#         for i in range(k):
-#             ans.append(heapq.heappop(kval)[1])
-            
-#         return ans
-        
         count = Counter(nums)
         priorityNum = []
         res = []
