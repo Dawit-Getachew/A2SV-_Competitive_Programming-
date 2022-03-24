17c96dd63b7a44c69bd89d96b95dfe948099c06a
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-03-24 23:55:58 +0300

    Time: 56 ms (55.23%), Space: 13.8 MB (73.77%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,12 +1,11 @@
 class Solution:
     def partitionLabels(self, S: str) -> List[int]:
-        chrPos = {}
-        for index, char in enumerate(S):      
-            chrPos[char] = index
+        charPosition, result = {}, []
         left, right = 0, 0
-        result = []
+        for index, char in enumerate(S):      
+            charPosition[char] = index
         for index, char in enumerate(S):
-            right = max(right, chrPos[char])
+            right = max(right, charPosition[char])
             if right == index:
                 result.append(right - left + 1)
                 left = index + 1
