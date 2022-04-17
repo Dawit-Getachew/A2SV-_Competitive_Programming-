80524526c9be1808a7a1bb88e9f240d0e918cb36
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-04-17 03:29:26 +0300

    Time: 55 ms (96.14%), Space: 14 MB (49.88%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,6 +1,7 @@
 class Solution:
     def minCostClimbingStairs(self, costs: List[int]) -> int:
-        pre , curr = 0, 0
+        pre = 0
+        curr = 0
         for cost in costs:
             pre, curr = curr, cost + min(curr,min(pre,curr))
         return min(pre, curr)
