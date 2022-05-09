ff958415ae71ee2d9bd309a8ee0542a461913e1e
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-05-09 18:12:05 +0300

    Time: 40 ms (66.93%), Space: 13.8 MB (98.61%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -2,9 +2,17 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if not needle:
             return 0
-        haystackList = list(haystack)
-        needleList = list(needle)
-        for i in range(len(haystackList)):
-            if "".join(haystackList[i:i+len(needle)]) == needle:
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
                 return i
         return -1
+        
+        
+        # if not needle:
+        #     return 0
+        # haystackList = list(haystack)
+        # needleList = list(needle)
+        # for i in range(len(haystackList)):
+        #     if "".join(haystackList[i:i+len(needle)]) == needle:
+        #         return i
+        # return -1
