3b8941a200bff64334d674741e71344702e1ad4a
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-05-09 17:37:14 +0300

    Time: 44 ms (58.04%), Space: 14 MB (51.77%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.py
@@ -0,0 +1,10 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        haystackList = list(haystack)
+        needleList = list(needle)
+        for i in range(len(haystackList)):
+            if "".join(haystackList[i:i+len(needle)]) == needle:
+                return i
+        return -1
