967a7c1abf9beb92d01e23cbba4fc5043af5d3f6
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-10 15:00:00 +0300

    Time: 88 ms (35.18%), Space: 14.5 MB (67.19%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashMapS = defaultdict(int)
+        hashMapT = defaultdict(int)
+        for i in s:
+            hashMapS[i] += 1
+        for i in t:
+            hashMapT[i] += 1
+        print(hashMapS)
+        print(hashMapT)
+        for i in hashMapS:
+            if hashMapS[i] == hashMapT[i]:
+                continue
+            else:
+                return False
+        for i in hashMapT:
+            if hashMapS[i] == hashMapT[i]:
+                continue
+            else:
+                return False
+        return True
+            
