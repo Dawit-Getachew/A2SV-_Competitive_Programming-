56ab34aa53927e38ef0cf74767753348280f5c35
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-11 11:41:57 +0300

    Time: 37 ms (81.84%), Space: 13.9 MB (51.74%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -1,18 +1,12 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        hashMap = defaultdict(int)
+        # hashMap = defaultdict(int)
+        # for i in s:
+        #     hashMap[i] += 1
+        #     if hashMap[i] == 2:
+        #         return i
+
+        Set = set()
         for i in s:
-            hashMap[i] += 1
-            if hashMap[i] == 2:
-                return i
-
-
-
-
-
-# class Solution:
-#     def repeatedCharacter(self, s: str) -> str:
-#         Set = set()
-#         for i in s:
-#             if i in Set: return i
-#             Set.add(i)
+            if i in Set: return i
+            Set.add(i)
