d088dd37508f682cef85df85dea4607b218f6c25
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-15 19:48:36 +0300

    Time: 55 ms (34.52%), Space: 13.8 MB (71.12%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -3,23 +3,23 @@ class Solution:
         stack = []
         def push(item):
             stack.append(item)
-        def Remove(stack):
+        def Remove():
             if stack:
                 stack.pop()
             return
-        def peak(stack):
+        def peak():
             if stack:
                 return stack[-1]
         newS = {"(","[","{"}
         for i in s:
             if i in newS:
                 stack.append(i)
-            elif i == ")" and peak(stack) == "(":
-                Remove(stack)
-            elif i == "]" and peak(stack) == "[":
-                Remove(stack)
-            elif i == "}" and peak(stack) == "{":
-                Remove(stack)
+            elif i == ")" and peak() == "(":
+                Remove()
+            elif i == "]" and peak() == "[":
+                Remove()
+            elif i == "}" and peak() == "{":
+                Remove()
             else:
                 return False
         return False if len(stack) != 0 else True
