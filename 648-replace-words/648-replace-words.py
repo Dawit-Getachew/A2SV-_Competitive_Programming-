57d6085c22ee1aeb0d2b87222fd28db3e740647b
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-16 13:02:53 +0300

    Time: 436 ms (27.49%), Space: 22.5 MB (67.61%) - LeetHub

--- 648-replace-words/648-replace-words.py
+++ 648-replace-words/648-replace-words.py
@@ -1,38 +1,10 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.isWord = False
-
-class Solution:
-    def __init__(self):
-        self.root = TrieNode()
-        
-    def insert(self, word) -> None:
-        curr = self.root
-        for char in word:
-            if char not in curr.children:
-                curr.children[char] = TrieNode()
-            curr = curr.children[char]
-        curr.isWord = True
-        
-    def search(self, sent):
-        curr = self.root
-        lis = []
-        flag = False
-        for char in sent:
-            if char in curr.children:
-                curr = curr.children[char]
-                lis.append(char)
-                if curr.isWord == True:
-                    return ''.join(lis)
-            else: return False
-            
+class Solution:            
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        sentence =  sentence.split()
         for word in dictionary:
-            self.insert(word)
-        sentence = sentence.split()
-        for i in range(len(sentence)):
-            res = self.search(sentence[i])
-            if res:
-                sentence[i] = res
+            for i in range(len(sentence)):
+                if word[0] != sentence[i][0]:
+                    continue
+                if len(sentence) >= len(word) and word == sentence[i][:len(word)]:
+                    sentence[i] = word
         return ' '.join(sentence)
