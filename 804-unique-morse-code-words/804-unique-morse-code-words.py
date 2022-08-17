d6077ab7e0469a8313cfdea08c2d0b530489017a
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-17 17:06:59 +0300

    Time: 62 ms (35.11%), Space: 13.9 MB (24.27%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.py
+++ 804-unique-morse-code-words/804-unique-morse-code-words.py
@@ -1,14 +1,5 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        res = set()
-        charMap = {}
-        morseCode = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        a = list(string.ascii_lowercase)
-        for i in range(len(morseCode)):
-            charMap[a[i]] = morseCode[i]
-        for word in words:
-            morseWord = ""
-            for char in word:
-                morseWord += charMap[char]
-            res.add(morseWord)
-        return len(res)
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        translate = {''.join([morse[ord(c)-97] for c in word]) for word in words}
+        return len(translate)
