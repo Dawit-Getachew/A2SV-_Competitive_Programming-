8a041ceafffb03c291a9b1be8edf1bad147cf44a
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-19 11:22:12 +0300

    Time: 1639 ms (5.01%), Space: 28 MB (31.21%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -3,16 +3,14 @@ class Solution:
         left, right, duplicate = 0, len(nums) - 1, 0
         while left <= right:
             mid = left + (right - left)//2
-            count = self.countSmaller(nums, mid)
+            count = 0
+            for i in range(len(nums)):
+                if nums[i] <= mid:
+                    count += 1
             if count > mid:
                 duplicate = mid
                 right = mid - 1
             else:
                 left = mid + 1
         return duplicate
-    def countSmaller(self, nums, target):
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] <= target:
-                count += 1
-        return count
+        
