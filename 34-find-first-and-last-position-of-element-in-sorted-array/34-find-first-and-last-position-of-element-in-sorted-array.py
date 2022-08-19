99bae38f7e18e8bfeb963afdb9687d83280d199c
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-19 11:34:06 +0300

    Time: 132 ms (53.51%), Space: 15.4 MB (48.44%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,21 +1,23 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        return [self.search(nums, target, True), self.search(nums, target, False)]
-    def search(self, nums, target, first):
-        left = 0
-        right = len(nums)-1
-        ans = -1
+        left, right, rightAns, leftAns = 0, len(nums)-1, -1, -1
         while left <= right:
-            mid = left + (right - left)//2
+            mid = left + (right - left) // 2
             if nums[mid] == target:
-                ans = mid
-                if first:
-                    right = mid - 1
-                else:
-                    left = mid + 1
+                rightAns = mid
+                left = mid + 1
             elif nums[mid] < target:
                 left = mid + 1
-            elif nums[mid] > target:
+            elif  nums[mid] > target:
                 right = mid - 1
-        return ans
-                
+        left, right, leftAns = 0, len(nums)-1, -1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                leftAns = mid
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            elif  nums[mid] > target:
+                right = mid - 1
+        return [leftAns,rightAns]
