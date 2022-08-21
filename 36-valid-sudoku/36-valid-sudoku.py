d567396b6d7cf1ab4e942f3e4bd82cacf19d74bf
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-21 19:40:43 +0300

    Time: 98 ms (94.85%), Space: 14 MB (35.29%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,35 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        s = set()
-
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != '.':
-                    c = board[i][j]
-
-                    # Row check
-                    key = f'{c} in row {i}' # same as -> str(c) + ' in row ' + str(i)
-
-                    if key in s:
-                        return False
-                    else:
-                        s.add(key)
-
-                    # Column check
-                    key = f'{c} in col {j}' # same as -> str(c) + ' in col ' + str(j)
-
-                    if key in s:
-                        return False
-                    else:
-                        s.add(key)
-
-                    # Box check
-                    boxIndex = (i // 3) * 3 + (j // 3)
-                    key = f'{c} in box {boxIndex}' # same as -> str(c) + ' in box ' + str(boxIndex)
-
-                    if key in s:
-                        return False
-                    else:
-                        s.add(key) 
-
+        rs=collections.defaultdict(set)
+        cs=collections.defaultdict(set)
+        sq=collections.defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c]==".":
+                    continue
+                if board[r][c] in rs[r] or board[r][c] in cs[c] or board[r][c] in sq[(r//3,c//3)]:
+                    return False
+                rs[r].add(board[r][c])
+                cs[c].add(board[r][c])
+                sq[(r//3,c//3)].add(board[r][c])
         return True
