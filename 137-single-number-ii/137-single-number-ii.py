982046e43deb4cee162a26c83ffdc4237c3edc67
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-21 22:48:15 +0300

    Time: 88 ms (68.70%), Space: 16.1 MB (7.95%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,13 +1,3 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        for i in range(32):
-            count = 0
-            for num in nums:
-                count += (num >> i) & 1
-            rem = count % 3
-            if i == 31 and rem:
-                res -= 1 << 31
-            else:
-                res |= rem * (1 << i)
-        return res
+        return (sum(set(nums))*3 - sum(nums))//2
