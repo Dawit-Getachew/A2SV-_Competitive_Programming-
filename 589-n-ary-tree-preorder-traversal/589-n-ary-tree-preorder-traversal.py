3d429e74d86d01be455992b71d298ddb87c180d9
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-21 19:42:02 +0300

    Time: 84 ms (44.05%), Space: 16.4 MB (16.78%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        res  = []
+        def dfs(node):
+            if not node:
+                return None
+            res.append(node.val)
+            for nd in node.children:
+                dfs(nd)
+        dfs(root)
+        return res
