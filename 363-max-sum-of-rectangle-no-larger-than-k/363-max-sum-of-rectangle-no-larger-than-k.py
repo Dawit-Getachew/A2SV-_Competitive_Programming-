0d20c3628efde47141a2b816915caabea41f783d
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-28 00:06:45 +0300

    Time: 1857 ms (89.72%), Space: 14.8 MB (70.92%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
@@ -1,29 +1,23 @@
 class Solution:
     def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
-        def find(nums: List[int]):
-            prefixsums_encountered = [0]
-            n = 1
-            res = -inf
-            for prefixsum in accumulate(nums):
-                prev_sum = prefixsum - k
-                pos = bisect_left(prefixsums_encountered, prev_sum)
-                if pos < n:
-                    res = max(res, prefixsum - prefixsums_encountered[pos])
-                    if res == k:
-                        return k
-                insort(prefixsums_encountered, prefixsum)
-                n += 1
-            return res
-
         height, width = len(matrix), len(matrix[0])
         res = -inf
-        for rownum in range(height):
-            totals = [0] * width
-            for bottom_row in range(rownum, height):
+        for top_row in range(height):
+            nums = [0] * width
+            for bottom_row in range(top_row, height):
                 for colnum in range(width):
-                    totals[colnum] += matrix[bottom_row][colnum]
-
-                res = max(res, find(totals))
-                if res == k:
-                    return k
+                    nums[colnum] += matrix[bottom_row][colnum]
+                prefixsums_encountered = [0]
+                prefixsum = 0
+                for pos, num in enumerate(nums, 1):
+                    prefixsum += num
+                    complement = prefixsum - k
+                    complement_pos = bisect_left(prefixsums_encountered, complement)
+                    if complement_pos < pos:
+                        subarray_sum = prefixsum - prefixsums_encountered[complement_pos]
+                        if subarray_sum == k:
+                            return k
+                        elif subarray_sum < k:
+                            res = max(res, subarray_sum)
+                    insort(prefixsums_encountered, prefixsum)
         return res
