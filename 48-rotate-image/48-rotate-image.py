248028c5a6ed352ec5ba8cd0f7a27a176878c095
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-08-30 19:19:25 +0300

    Time: 54 ms (54.37%), Space: 14.1 MB (29.99%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,10 +3,17 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        y_dim = len(matrix)
-        x_dim = len(matrix[0])
-        for i in range(0, y_dim):
-            for j in range(i, x_dim):
-                matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
-        for i in range(0, y_dim):
-            matrix[i].reverse()
+        visited = set()
+        n = len(matrix)
+        for row in range(n):
+            for col in range(n):
+                if row == 0 and col == 0 or (row, col) in visited or (col, row) in visited:
+                    continue
+                a = matrix[row][col]
+                b = matrix[col][row]
+                matrix[row][col] = b
+                matrix[col][row] = a
+                visited.add((row, col))
+                visited.add((col, row))
+        for row in matrix:
+            row.reverse()
