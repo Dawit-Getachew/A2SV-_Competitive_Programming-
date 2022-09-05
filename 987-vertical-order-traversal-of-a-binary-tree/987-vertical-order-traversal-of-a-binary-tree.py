f32bc4b4aec475fd6f3b0ed8d5a57f04b8bb4e5c
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-09-05 07:34:49 +0300

    Time: 43 ms (77.22%), Space: 14.2 MB (28.68%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,10 +6,24 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        d = defaultdict(list)
-        def dfs(r, row, col):
-            d[col].append((row, r.val))
-            if r.left: dfs(r.left, row+1, col-1)
-            if r.right: dfs(r.right, row+1, col+1)
-        dfs(root, 0, 0)
-        return [[e for _, e in sorted(v)] for k, v in sorted(d.items())]
+        col_rows = defaultdict(list)
+
+        queue = deque()
+        queue.append((root, 0, 0))
+        while queue:
+            node_x_y = queue.popleft()
+            node, x, y = node_x_y[0], node_x_y[1], node_x_y[2]
+            col_rows[y].append((x, node.val))
+            if node.left:
+                queue.append((node.left, x + 1, y - 1))            
+            if node.right:
+                queue.append((node.right, x + 1, y + 1))
+
+        rst = []
+        min_col, max_col = min(col_rows.keys()), max(col_rows.keys())
+        for col in range(min_col, max_col + 1):
+            if col not in col_rows:
+                continue
+            col_rows[col].sort()
+            rst.append([t[1] for t in col_rows[col]])
+        return rst
