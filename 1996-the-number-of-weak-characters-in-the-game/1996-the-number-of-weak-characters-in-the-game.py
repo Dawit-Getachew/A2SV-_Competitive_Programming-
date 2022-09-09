9b44d8570918da9cf81565db9d1305ca04d93541
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-09-09 22:50:15 +0300

    Time: 4664 ms (11.35%), Space: 66.5 MB (91.64%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -1,17 +1,25 @@
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
-        maxAttack = max(a for a, _ in properties)
-        maxDefense = [0] * (maxAttack+2)
+        maxAttackValue = 0
+        minAttackValue = float("inf")
+        maxDefense = [0] * 100002
         
-        for a, b in properties:
-            maxDefense[a] = max(maxDefense[a], b)
+        for p in properties:
+            maxDefense[p[0]] = max(maxDefense[p[0]], p[1])
+            minAttackValue = min(minAttackValue, p[0])
+            maxAttackValue = max(maxAttackValue, p[0])
         
-        for i in reversed(range(len(maxDefense)-1)):
-            maxDefense[i] = max(maxDefense[i], maxDefense[i+1])
+        maxDefenseValue = 0
         
-        ans = 0
-        for a, b in properties:
-            if b < maxDefense[a+1]:
-                ans += 1
-        return ans
-    
+        for i in range(maxAttackValue, minAttackValue - 1, -1):
+            if maxDefenseValue > maxDefense[i]:
+                maxDefense[i] = maxDefenseValue
+            else:
+                maxDefenseValue = maxDefense[i]
+        
+        res = 0
+        
+        for p in properties:
+            if maxDefense[p[0] + 1] > p[1]:
+                res += 1
+        return res
