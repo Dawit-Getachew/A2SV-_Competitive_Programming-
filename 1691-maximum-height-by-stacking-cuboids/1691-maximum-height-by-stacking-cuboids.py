d2d6f2ff76d343a21374510caec3b8da6aa5f0c1
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-09-09 22:48:02 +0300

    Time: 147 ms (63.01%), Space: 13.9 MB (86.71%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.py
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.py
@@ -1,23 +1,17 @@
 class Solution:
-    def maxHeight(self, A: List[List[int]]) -> int:
-        n = len(A)
-        for c in A: c.sort()
-
-        g = [set() for _ in range(n)]  # a->b means a<=b
-        din = [0] * n  # in_degree
-        for i in range(n):
-            for j in range(n):
-                if i != j and all(di <= dj for di, dj in zip(A[i], A[j])) and i not in g[j]:
-                    g[i].add(j)
-                    din[j] += 1
-
-        hmx = [h for _, _, h in A]  # max heights
-
-        que = collections.deque([i for i in range(n) if din[i] == 0])
-        while que:  # topological sort
-            p = que.popleft()
-            for q in g[p]:
-                hmx[q] = max(hmx[q], hmx[p] + A[q][2])
-                din[q] -= 1
-                if din[q] == 0: que.append(q)
-        return max(hmx)
+    def maxHeight(self, cuboids: List[List[int]]) -> int:
+        for i in range(len(cuboids)):
+            cuboids[i]=sorted(cuboids[i])
+        cuboids=sorted(cuboids)    
+        #print(cuboids) 
+        
+        dp=[0]*(len(cuboids))
+        for i in range(0,len(cuboids)):
+            dp[i]=cuboids[i][-1]
+            
+        for i in range(1,len(cuboids)):
+            for j in range(i):
+                if cuboids[i][0]>=cuboids[j][0] and cuboids[i][1]>=cuboids[j][1] and cuboids[i][2]>=cuboids[j][2] and dp[i]<dp[j]+cuboids[i][2]:
+                    dp[i]=dp[j]+cuboids[i][2]
+                    
+        return max(dp)   
