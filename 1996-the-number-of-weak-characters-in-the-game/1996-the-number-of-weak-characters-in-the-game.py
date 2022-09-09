bbfc4a560ca91fa3410c463e66b72262be936aa6
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-09-09 22:50:56 +0300

    Time: 2305 ms (93.74%), Space: 67 MB (78.82%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -1,25 +1,9 @@
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
-        maxAttackValue = 0
-        minAttackValue = float("inf")
-        maxDefense = [0] * 100002
-        
-        for p in properties:
-            maxDefense[p[0]] = max(maxDefense[p[0]], p[1])
-            minAttackValue = min(minAttackValue, p[0])
-            maxAttackValue = max(maxAttackValue, p[0])
-        
-        maxDefenseValue = 0
-        
-        for i in range(maxAttackValue, minAttackValue - 1, -1):
-            if maxDefenseValue > maxDefense[i]:
-                maxDefense[i] = maxDefenseValue
-            else:
-                maxDefenseValue = maxDefense[i]
-        
-        res = 0
-        
-        for p in properties:
-            if maxDefense[p[0] + 1] > p[1]:
-                res += 1
-        return res
+        properties.sort(key=lambda x:(-x[0],x[1]))
+        count=maxe=0
+        for a,d in properties:
+            if d<maxe:
+                count+=1
+            maxe=max(maxe,d)
+        return count
