8963c58a07935a19a438fcab6b14dad28aff9122
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-09-11 21:23:04 +0300

    Time: 82 ms (97.07%), Space: 13.8 MB (99.73%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,17 +1,9 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        profits, vp = [], []
-
-        ps = [1000] + prices + [0]
-        for v, p in pairwise(b for a, b, c in zip(ps, ps[1:], ps[2:]) if (a < b) ^ (b < c)):
-            while vp and (v < vp[-1][0] or p >= vp[-1][1]):
-                pv, pp = vp.pop()
-                if pv <= v:
-                    v, pv = pv, v
-                if pp > pv:
-                    profits.append(pp - pv)
-            vp.append((v, p))
-        else:
-            profits.extend(p - v for v, p in vp)
-
-        return sum(nlargest(k, profits))
+        buy = [-inf]*(k+1)
+        sell = [0] *(k+1)
+        for price in prices:
+            for i in range(1,k+1):
+                buy[i] = max(buy[i],sell[i-1]-price)
+                sell[i] = max(sell[i],buy[i]+price)
+        return sell[-1]
