cee24c9acf79051d55e7a6734a6d9d527f58d7fa
Dawit-Getachew <69157562+Dawit-Getachew@users.noreply.github.com>
2022-09-13 20:11:04 +0300

    Time: 223 ms (31.43%), Space: 14.2 MB (35.67%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.py
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -1,25 +1,22 @@
 class Solution:
     def validUtf8(self, data: List[int]) -> bool:
         count = 0
-        for n in data:
-            if n & 127 == n: #127 = 01111111
-                if count: 
+        for byte in data:
+            if 128 <= byte <= 191:
+                if count <= 0:
                     return False
-                continue
-            if n & 191 == n: #191 = 10111111
-                if count: 
-                    count -= 1
+                count -= 1
+            else:
+                if count:
+                    return False
+                if byte < 128:
                     continue
-                return False
-            if count: 
-                return False
-            if n & 223 == n: #223 = 11011111
-                count = 1
-
-            elif n & 239 == n: #239 = 11101111
-                count = 2
-            elif n & 247 == n: #247 = 11110111
-                count = 3
-            else: #(n > 247) OR (n & 248 == 248) #248 = 11111000
-                return False
+                elif byte < 224:
+                    count = 1
+                elif byte < 240:
+                    count = 2
+                elif byte < 248:
+                    count = 3
+                else:
+                    return False
         return count == 0
